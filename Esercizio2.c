af72ad1704acec90b14dbdc2bd316d00f468483f
Tetiana Kizan <t.kizan@studenti.unina.it>
2020-03-31 17:26:15 +0200

    Prova a fare esercizio 2 della lezione 14

---
+++ Esercizio2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#define MAXLEN 30
+
+struct esame {
+char esame;
+int voto;
+};
+
+int main() {
+
+  struct esame numEsami[MAXLEN];
+  int i, n;
+
+  printf("Inserisci numero di esami che ha fatto lo studente: ");
+  scanf("%d", &n);
+
+  for(i = 0; i < n; ++i){
+    printf("Esame e voto dello studente: ");
+    scanf("%s %d", numEsami[i].esame, &(numEsami[i].voto));
+  }
+
+
+  printf("\nInserisci l'esame da cercare: ");
+  scanf("%s", numEsami[i].esame);
+
+  printf("Il voto dell'esame  %s è: %d",  numEsami[i].esame, &(numEsami[i].voto));
+
+  printf("\nL'esame non è presente.");
+}
