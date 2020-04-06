0134e2a090b72ab9881f92e2edade339b4fc4bda
Tetiana Kizan <t.kizan@studenti.unina.it>
2020-04-06 16:40:20 +0200

    create esercizio3

---
+++ Esercizio3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#define MAXLEN 50
+
+struct studente {
+char cognome;
+int votoBasso;
+int votoAlto;
+float mediaVoti;
+};
+
+int main() {
+  struct studente s[MAXLEN];
+  printf("Inserisci qualche n: ");
+  scanf("%d", &n);
+}
