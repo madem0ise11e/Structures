dbd055b0597f575830e21b1ba94ab96ab3cadb27
Tetiana Kizan <t.kizan@studenti.unina.it>
2020-04-07 19:33:49 +0200

    adjust

--- Esercizio3.c
+++ Esercizio3.c
@@ -11,12 +11,12 @@ float mediaVoti;
 int menu();
 void riempiArrayDati(struct studente s[], int n);
 void stampaArrayDati(struct studente s[], int n);
-//void cercaEsame(struct esame e[], int n);
+void cercaMedia(struct studente s[], int n);
 
 int main() {
   struct studente s[MAXLEN];
   int n, selezione, i;
-  printf("Quanti studenti vuoi inserire (min 5): ");
+  printf("Quanti studenti vuoi inserire? ");
   scanf("%d", &n);
   printf("Riempi i dati per gli studenti:\n");
   riempiArrayDati(s, n);
@@ -25,21 +25,18 @@ int main() {
     printf("\n\nProgramma menu a selezione\n");
       do {
       printf("\nDigita il numero corrispondente:\n");
-      printf("1 per visualizzare i dati dei 5 studenti\n2 per vedere i dati dello studente con la media piu' alta\n4 per uscire\n\n");
+      printf("1 per visualizzare i dati dei %d studenti\n2 per vedere i dati dello studente con la media piu' alta\n4 per uscire\n\n", n);
       printf("Selezione: ");
       scanf("%d", &selezione);
       switch(selezione) {
        case 1:
 
-        printf("\nDati dei 5 studenti: \n");
+        printf("\nDati dei %d studenti: \n", n);
         stampaArrayDati(s,n);
           break;
        case 2:
-        printf("\nDati dello studente con la media piu' alta: \n\n");
-      //  for(i=0; i<n; i++){
-      //  if(s[i].mediaVoti)
-          }
-          break;
+        cercaMedia(s,n);
+       break;
        case 4:
        	printf (" USCITA \n");
       break;
@@ -79,3 +76,13 @@ void stampaArrayDati(struct studente s[], int n){
       printf("Studente %s ha il voto piu' alto %d, voto piu' basso %d, media voti %f\n", s[i].cognome, s[i].votoAlto, s[i].votoBasso, s[i].mediaVoti);
     }
   }
+
+void cercaMedia(struct studente s[], int n){
+  int i, largest;
+  largest = s[0].mediaVoti;
+  for(i=0; i<n; ++i){
+    if (largest < s[i].mediaVoti)
+    largest = s[i].mediaVoti;
+  }
+  printf("La media piu' alta e' %d. \n", largest);
+}
